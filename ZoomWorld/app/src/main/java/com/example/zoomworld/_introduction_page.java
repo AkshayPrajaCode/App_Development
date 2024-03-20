915f229602b5bbd14a497b346dbe1bad4287c4ca
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-03-20 15:05:23 +0530

    Application Ready Now

---
+++ ZoomWorld/app/src/main/java/com/example/zoomworld/_introduction_page.java
@@ -0,0 +1,14 @@
+package com.example.zoomworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class _introduction_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction_page);
+    }
+}
