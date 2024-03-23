130c53f6810a48ed0645eee8908163cc4a32e4b1
AkshayPrajaCode <akshaykumarprajapati@hotmail.com>
2024-03-23 19:31:47 +0530

    Logcat Problem

--- ZoomWorld/app/src/main/java/com/example/zoomworld/data/MyDataBaseHandler.java
+++ ZoomWorld/app/src/main/java/com/example/zoomworld/data/MyDataBaseHandler.java
@@ -2,31 +2,91 @@ package com.example.zoomworld.data;
 
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.zoomworld.Model.Contact;
 import com.example.zoomworld.Parameters.parameter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyDataBaseHandler extends SQLiteOpenHelper {
 
 
-    public MyDataBaseHandler(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public MyDataBaseHandler(Context context) {
+        super(context, parameter.DATA_BASE_NAME, null, parameter.DATA_BASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String Create = "Create Table "+ parameter.DATA_BASE_TABLE +"("+
-                parameter.KEY_ID + "Integer Primary Key , " + parameter.KEY_name + "Text , " + parameter.KEY_phone_number + " Text , "
-                +")";
+        String Create = "Create Table " + parameter.DATA_BASE_TABLE + "(" +
+                parameter.KEY_ID + "Integer Primary Key , " + parameter.KEY_name + "Text , " + parameter.KEY_phone_number + " Text "
+                + ")";
+        Log.d("TAG", "onCreate: Query is runing " + Create);
+        db.execSQL(Create);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addContact(Contact contact) {
+        SQLiteDatabase DB = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(parameter.KEY_name, contact.getName());
+        values.put(parameter.KEY_phone_number, contact.getPhoneNumber());
+
+
+        /********************* Insert ***************************/
+        DB.insert(parameter.DATA_BASE_TABLE, null, values);
+        Log.e("TAG", "addContact: Successfylly inserted");
+        Log.d("TAG", "addContact: Successfylly inserted ");
+        DB.close();
+
+    }
+
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        /************************************ Generate the query to read from the database *****************************/
+        String select = "SELECT * FROM " + parameter.DATA_BASE_TABLE;
+        Cursor cursor = db.rawQuery(select, null);
+
+
+        /********************************** loop throught now **********************************************************/
+        if (cursor.moveToFirst()){
+            do {
+                Contact contact= new Contact();
+                contact.setId(Integer.parseInt(cursor.getString(0)));
+                contact.setName(cursor.getString(1));
+                contact.setPhoneNumber(cursor.getString(2));
+                contactList.add(contact) ;
+            }while (cursor.moveToFirst());
+        }
+        return contactList;
+    }
+
+    public int updateContact(Contact contact){
+        SQLiteDatabase database=this.getWritableDatabase();
+        ContentValues values= new ContentValues();
+        values.put(parameter.KEY_name,contact.getName());
+        values.put(parameter.KEY_phone_number,contact.getPhoneNumber());
+
+
+        //Lets update now
+
+        return database.update(parameter.DATA_BASE_TABLE,values,parameter.KEY_ID+"?",
+                new String []{String.valueOf(contact.getId())});
     }
 }
--- ZoomWorld/app/src/main/java/com/example/zoomworld/data_base_activity.java
+++ ZoomWorld/app/src/main/java/com/example/zoomworld/data_base_activity.java
@@ -4,8 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.zoomworld.Model.Contact;
+import com.example.zoomworld.data.MyDataBaseHandler;
+
+import java.util.List;
 
 public class data_base_activity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,45 @@ public class data_base_activity extends AppCompatActivity {
         setContentView(R.layout.activity_data_base);
         getWindow().setStatusBarColor(Color.parseColor("#000000"));
         getWindow().setNavigationBarColor(Color.parseColor("#FADBD8"));
+
+
+        MyDataBaseHandler myDataBaseHandler= new MyDataBaseHandler(data_base_activity.this);
+
+        /****************************** Creating  a contact to the Database *************************************/
+
+        Contact Mahta= new Contact();
+        Mahta.setName("Amardeep Mahta");
+        Mahta.setPhoneNumber("6201145983");
+
+        /******************************* Adding   a contact to the Database *************************************/
+
+        myDataBaseHandler.addContact(Mahta);
+
+        Contact Amardeep = new Contact();
+        Amardeep.setName("Akshay Kumar Prajapati");
+        Amardeep.setPhoneNumber("8987918309");
+        myDataBaseHandler.addContact(Amardeep);
+
+        Amardeep.setName("Ajay Kumar Prajapati");
+        Amardeep.setPhoneNumber("91232523987");
+        int affectedRows=myDataBaseHandler.updateContact(Amardeep);
+
+
+        Log.d("TAG", "onCreate: No of affected Rows are : "+affectedRows);
+
+
+        Log.e("TAG", "onCreate: Succesfully Add the Contact in the DataBase" );
+        Toast.makeText(this, "Succesfully Add the Contact in the DataBase", Toast.LENGTH_SHORT).show();
+
+        /******************************************** Get all contacts *********************************************/
+        List<Contact> allContacts= myDataBaseHandler.getAllContacts();
+
+        /************************************** Using  for looop *************************************/
+
+
+        for (Contact contact :
+                allContacts) {
+            Log.d("TAG", "id:"+ contact.getId()+"\n"+"Name "+ contact.getName()+"\n"+"Mobile Number "+contact.getPhoneNumber());
+        }
     }
 }
